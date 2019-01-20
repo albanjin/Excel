147012b6415bf370e641d1282d7a57bc22619bcf
albanjin <albanjin@163.com>
2019-01-20 23:26:00 +0800

    first commit

--- requestHtml.py
+++ requestHtml.py
@@ -7,8 +7,6 @@ html = BeautifulSoup(response,'html.parser')
 target_arr  =  html.select('#list .item.server-item>a')
 target_hot = html.select('.hot > .item > a')
 target_arr.extend(target_hot)
-# for val in target_arr :
-#     print(val.attrs['href'])
 
 for val in target_arr :
     print(val.attrs['href'])
@@ -16,4 +14,3 @@ for val in target_arr :
     resp = writeContent.get_html_data(val.attrs['href'])
     writeContent.create_html(resp.decode('utf-8'),val.attrs['href'].replace('https://www.meipian.cn/',''))
 
-# writeContent.create_file_func(response.content.decode('utf-8'))
