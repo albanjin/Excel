953f7acbe5733151a6ea87012994432eb047a582
albanjin <albanjin@163.com>
2019-01-20 23:25:01 +0800

    first commit

--- requestHtml.py
+++ requestHtml.py
@@ -1,21 +1,11 @@
-
-
 from common import writeContent
 from bs4 import BeautifulSoup
-
-#要抓取的目标页码地址
-
-
-#查看响应状态码
-# status_code = response.status_code
 
 #使用BeautifulSoup解析代码,并锁定页码指定标签内容
 response = writeContent.get_html_data()
 html = BeautifulSoup(response,'html.parser')
-# find_all(['a','.item.server-item'])
 target_arr  =  html.select('#list .item.server-item>a')
 target_hot = html.select('.hot > .item > a')
-# print(target_hot)
 target_arr.extend(target_hot)
 # for val in target_arr :
 #     print(val.attrs['href'])
