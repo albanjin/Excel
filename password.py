c041172130bca48c5be2c2415e8459ab1ce35d85
albanjin <albanjin@163.com>
2019-02-01 16:24:15 +0800

    update:新增面试题1

--- password.py
+++ password.py
@@ -8,3 +8,29 @@
 
     # script :
     # let reg = /\w{6,20}/ 
+
+#  已知1998年是平年，1.2号星期五，要求输入一个年份（大于1998） ，能够判断是否闰年，并返回该年份中即是周五又是13号的月份的个数
+import time , datetime
+# print(time.time())
+# tss1 = '1998-01-02 23:40:00'
+# 转为时间数组
+# timeArray = time.strptime(tss1, "%Y-%m-%d %H:%M:%S")
+# print(timeArray)
+def is_leap_year (num) :
+
+    year = int (num)
+    count_mon = 0
+    is_leap = '平年'
+
+    if(year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+        is_leap = '闰年'
+
+    for m in range(1,13):
+        year_str = '{0}-{1}-13'.format(year,m)
+        timeArray = time.strptime(year_str, "%Y-%m-%d")
+        if timeArray[6] == 4 :
+            count_mon += 1
+    print('{0}是{1},周五又是13号的月份的个数{2}'.format(year,is_leap,count_mon))
+
+year_string = input('请输入年')
+is_leap_year(year_string)
